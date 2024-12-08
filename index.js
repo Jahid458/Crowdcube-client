f981385b6d640f7984f50e1e1ffaf4704496592b
Jahid458 <jahidul898@outlook.com>
2024-12-08 17:12:12 +0600

    update data

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json())
 
-//crowdcube
-//iwlU6tCLY3tG0Vqx
+
 
 
 
@@ -30,7 +29,7 @@ async function run() {
   // ghdfsghdsdgdg
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     
     app.get('/campaigns/:id', async(req,res) =>{
       const id = req.params.id; 
@@ -40,13 +39,17 @@ async function run() {
     })
 
 
-
+    // campaign card show
     app.get('/campaigns', async(req,res) =>{
         const cursor = database.find();
         const result = await cursor.toArray(); 
         res.send(result)
     })
 
+   
+
+    
+  
     app.get('/homecampaign', async(req,res) =>{
         const cursor = database.find().limit(6);
         const result = await cursor.toArray(); 
@@ -54,31 +57,54 @@ async function run() {
     })
 
     app.get('/mycampaign', async(req,res)=>{
-      const userEmail = req.query.email ; 
+      const userEmail = req.query.email; 
       console.log(userEmail)
       const query = {userEmail: userEmail} ; 
       const result = await database.find(query).toArray()
       console.log(result)
       res.send(result)
-
     })
 
     app.get('/mycampaign/:email', async(req,res)=>{
-      const userEmail = req.params.email ; 
+      const userEmail = req?.params.email ; 
       const query = {userEmail: userEmail} ; 
-      const result = await database.find(query).toArray()
+      const result = await database2.find(query).toArray()
       res.send(result)
     })
 
-  
 
+    app.put('/mycampaign/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const updatedUser = req.body;
+      const User = {
+        $set:{
+          imageURL:updatedUser.imageURL,
+          campaignTitle:updatedUser.campaignTitle,
+          campaignType:updatedUser.campaignType,
+          description:updatedUser.description,
+          minDonation:updatedUser.minDonation,
+          deadline:updatedUser.deadline,
+          userEmail:updatedUser.userEmail,
+          userName:updatedUser.userName
+        }
+      }
+      const result = await database.updateOne(filter,User,options);
+      res.send(result)
+    })
+
+
+    app.delete("/mycampaign/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }; 
+      const result = await database.deleteOne(query);
+      res.send(result);
+    });
     
 
-
-
-
-    // imageURL,campaignTitle,campaignType,description,minDonation,deadline
-    app.post('/campaigns',async(req,res)=>{
+    app.post('/campaigns',async(req,res)=>{ 
+       
         const newCampaign = req.body; 
         console.log("Successfully new campaign added", newCampaign); 
         const result = await database.insertOne(newCampaign);
@@ -97,7 +123,7 @@ async function run() {
     const database = client.db("crowdcube").collection("campaignList");
     const database2 = client.db("crowdcube").collection("donationdetails");
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
